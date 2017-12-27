7b67aa93296cdc88467ccb5bf459bbfbdf76498b
Arturo Arranz Mateo <arranz.91@gmail.com>
2017-12-27 14:25:17 +0100

    functional header with tables

--- publisher.py
+++ publisher.py
@@ -1,10 +1,18 @@
 import os
 import pdb
 
+def post_preview(file_name,path):
+    with open(path+file_name,'r') as fp:
+        data = fp.read()
+        return "<a href='/personal_page/%s' class='post_preview'>%s...</a>"%(path+file_name,data[:60])
+
 with open('blog/base.html','r+') as fp:
     html_index = fp.read()
     arr = os.listdir("blog/")
-    html_index = html_index.replace("{% list_of_posts %}","".join([ "<li><a href='personal_page/blog/%s'>%s</a></li>"%(entry,entry) for entry in arr if entry != 'index.html' and entry != 'base.html']))   
+    list_previews = "".join([post_preview(entry,'blog/') for entry in arr if
+                             entry.find("index")==-1 and
+                             entry.find("base")==-1])
+    html_index = html_index.replace("{% list_of_posts %}",list_previews)   
     
     with open('blog/index.html','w+') as fi:
         fi.write(html_index)
